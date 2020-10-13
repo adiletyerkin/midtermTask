b30afd2cd3ef278257b30812ab042fef46bcb0a0
Adilet Yerkin <adilet.yerkin@gmail.com>
2020-10-14 00:01:23 +0600

    Initial Commit

---
+++ MidtermTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MidtermTask
+//
+//  Created by Macbook Pro 2020 on 10/14/20.
+//  Copyright © 2020 Macbook Pro 2020. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
